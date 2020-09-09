45886af192f91fe43c0aa36b19705dc1a779d629
Анатолий Васильев <vas11yev.work@ya.ru>
2020-09-09 23:56:40 +0300

    first commit

---
+++ build/precache-manifest.4aabfb6c4fad54b4b1e5717d687cf5d1.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0778070eecd7a754a6b7db606026b508",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f5732a750347a26f2986",
+    "url": "/static/css/main.1feeef33.chunk.css"
+  },
+  {
+    "revision": "6e40983842f43eb41f05",
+    "url": "/static/js/2.7e1ee891.chunk.js"
+  },
+  {
+    "revision": "41b66b6fd6eecba581340794bb1831be",
+    "url": "/static/js/2.7e1ee891.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f5732a750347a26f2986",
+    "url": "/static/js/main.8590ecce.chunk.js"
+  },
+  {
+    "revision": "df11726cbf66339790ea",
+    "url": "/static/js/runtime-main.970cab50.js"
+  },
+  {
+    "revision": "dfc54362fc2b0f7b99a4038b87dd9e64",
+    "url": "/static/media/1.dfc54362.png"
+  },
+  {
+    "revision": "2048975925bd8553d8aaa5e1d9d1ffb1",
+    "url": "/static/media/2.20489759.png"
+  },
+  {
+    "revision": "dbf82c38432d45de3c1c99f13c8b5fee",
+    "url": "/static/media/3.dbf82c38.png"
+  },
+  {
+    "revision": "e671d53961f44185ee61256a0e6f13a5",
+    "url": "/static/media/4.e671d539.png"
+  },
+  {
+    "revision": "35bcd06768ffafae4a5721b8f0c4fd8f",
+    "url": "/static/media/burger.35bcd067.png"
+  },
+  {
+    "revision": "d9b1d2c9ce1a863135396014ed1bc1e4",
+    "url": "/static/media/check-symbol.d9b1d2c9.svg"
+  },
+  {
+    "revision": "a352f469e2a37b0c05f8219c265bc32a",
+    "url": "/static/media/edit.a352f469.svg"
+  },
+  {
+    "revision": "4fd2ba47b4f59d0a4382a8d81aae85d6",
+    "url": "/static/media/kfc.4fd2ba47.png"
+  },
+  {
+    "revision": "60464810e8cfcde7a20fc19284c38a0e",
+    "url": "/static/media/mcdac.60464810.png"
+  },
+  {
+    "revision": "c4cb16671dad6949b9c662028ebc1fc5",
+    "url": "/static/media/refresh-button.c4cb1667.svg"
+  },
+  {
+    "revision": "4eb05fdd9f3d81f2f81cac5847e25cc9",
+    "url": "/static/media/sub.4eb05fdd.png"
+  }
+]);
